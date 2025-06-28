58abca49d7738d9e7de819933ed7ec861f2acf2c
Juan <juanfajardoreyes@hotmail.com>
2025-06-28 17:10:04 +0200

    Primer commit

---
+++ src/main/java/com/repositoryehr/RepositoryEhrApplication.java
@@ -0,0 +1,13 @@
+package com.repositoryehr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryEhrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoryEhrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/repositoryehr/RepositoryEhrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repositoryehr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryEhrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
